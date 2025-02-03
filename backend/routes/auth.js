98d4ec27b7137079d4d6374ab46b3a0a858f8eb9
Robin MENESTRET <robin.menestret@cea.fr>
2025-02-03 19:04:43 +0100

    adding 5letters word db

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,10 +6,10 @@ const router = express.Router();
 
 // Inscription
 router.post('/register', async (req, res) => {
-  console.log("il se passe quelque chose");
+  console.log("registering user", req.body);
   const { email, username, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
-  
+  console.log("data encoded")
   try {
     const result = await db.query(
       'INSERT INTO users (email, username, password) VALUES ($1, $2, $3) RETURNING id',
@@ -17,7 +17,8 @@ router.post('/register', async (req, res) => {
     );
     res.status(201).json({ message: 'User registered', userId: result.rows[0].id });
   } catch (error) {
-    res.status(500).json({ error: 'Error registering user' });
+    console.error("PostgreSQL Error : ", error)
+    res.status(500).json({ error: 'Error registering user', details: error.message });
   }
 });
 
@@ -26,13 +27,16 @@ router.post('/login', async (req, res) => {
   const { email, password } = req.body;
 
   try {
+    // Get the user by the email (it's an id)
     const result = await db.query('SELECT * FROM users WHERE email = $1', [email]);
     const user = result.rows[0];
 
+    // Check if the password is invalid
     if (!user || !(await bcrypt.compare(password, user.password))) {
+      console.log("Invalid credentials")
       return res.status(401).json({ error: 'Invalid credentials' });
     }
-
+    // In case password is valid, it create a jwt token and send it to client
     const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
     res.json({ token });
   } catch (error) {
