7e5c0d6d47c6ebab159d41539bb436464086f1fd
RobinM <robin.menestret@gmail.com>
2025-02-11 23:12:02 +0100

    password forgot added

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -6,14 +6,14 @@ function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [invalidLogin, setInvalidLogin] = useState(false);
+  const [message, setMessage] = useState('');
   const navigate = useNavigate();
-  const API_URL = process.env.REACT_APP_API_URL 
-
+  const API_URL = process.env.REACT_APP_API_URL;
 
   useEffect(() => {
     const token = localStorage.getItem('token');
     if (token) {
-      navigate('/Settings');
+      navigate('/settings');
     }
   }, [navigate]);
 
@@ -22,7 +22,7 @@ function Login() {
     try {
       const response = await axios.post(API_URL + '/api/auth/login', { email, password });
       localStorage.setItem('token', response.data.token);
-      navigate('/Settings');
+      navigate('/settings');
     } catch (error) {
       console.error('Login failed', error);
       setInvalidLogin(true);
@@ -32,11 +32,35 @@ function Login() {
     }
   };
 
+  const handleForgotPassword = async () => {
+    if (!email) {      
+      setMessage('Please enter your email address');
+      setTimeout(() => {
+      setMessage('');
+    }, 3000);
+      return;
+    }
+
+    try {
+      const response = await axios.post(API_URL + '/api/auth/forgot-password', { email });
+      setMessage(response.data.message);
+      setTimeout(() => {
+        setMessage('');
+      }, 3000);
+    } catch (error) {
+      console.error('Error sending forgot password email', error);
+      setMessage('Error sending forgot password email');
+      setTimeout(() => {
+        setMessage('');
+      }, 3000);
+    }
+  };
+
   return (
     <div className="login module">
       <h1>Sign In</h1>
       <form onSubmit={handleSubmit}>
-        { invalidLogin ? <label className='invalid-login-label'>invalid email or password</label> : <p></p>}
+        {invalidLogin ? <label className='invalid-login-label'>Invalid email or password</label> : <p></p>}
         <input
           type="email"
           placeholder="Email"
@@ -52,9 +76,11 @@ function Login() {
           onChange={(e) => setPassword(e.target.value)}
           className="input"
         />
+        <label className="forgot-password" onClick={handleForgotPassword} >Forgot Password ?</label>
         <br />
         <button type="submit" className="button">Login</button>
       </form>
+      {message && <p>{message}</p>}
     </div>
   );
 }
