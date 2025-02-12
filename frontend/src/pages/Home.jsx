178dbeed306dc9b3a84412ceb1d21470d6be5f60
RobinM <robin.menestret@gmail.com>
2025-02-12 18:22:03 +0100

    modify home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
     <div>
       <div className="home">
         <h1>Welcome to Wordle Game</h1>
-        <p>This is a prototype of wordle game for Holis.</p>
+        <p>This is a prototype of wordle game.</p>
         <p>You can click Play to test the game and also create your own account to save your progress!</p>
         <br/>
         <h2>How to Play</h2>
